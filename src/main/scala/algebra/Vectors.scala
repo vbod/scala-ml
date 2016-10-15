e09273068dcf367028814bcc872d80a59b102e76
unknown <vbod@hotmail.com>
2016-10-15 15:18:38 +0200

    vectors

--- src/main/scala/algebra/Vectors.scala
+++ src/main/scala/algebra/Vectors.scala
@@ -7,9 +7,11 @@ trait Vector {
   def apply(i: Int): Double
 
   def iterator(): Iterator[(Int, Double)]
+
+  def iteratorOnActives(): Iterator[(Int, Double)]
 }
 
-case class DenseVector(private val data: Array[Double])
+class DenseVector private(private val data: Seq[Double])
   extends Vector {
 
   override def dimension: Int = data.length
@@ -27,22 +29,41 @@ case class DenseVector(private val data: Array[Double])
       ret
     }
   }
+
+  override def iteratorOnActives(): Iterator[(Int, Double)] = iterator()
 }
 
-case class SparseVector(override val dimension: Int, private val indices: Array[Int], private val data: Array[Double])
+object DenseVector {
+  def apply(data: Array[Double]): DenseVector = {
+    new DenseVector(data)
+  }
+}
+
+class SparseVector private(override val dimension: Int,
+                           private val indices: Seq[Int],
+                           private val data: Seq[Double])
   extends Vector {
 
-  require(indices.length < dimension && (indices.length == data.length),
-    "Indices and data should have the same length < dimension.")
-
   override def apply(i: Int): Double = {
-    indices.find(_ == i) match {
-      case Some(ii) => data(ii)
+    indices.indexOf(i) match {
+      case ii if ii >= 0 => data(ii)
       case _ => 0d
     }
   }
 
   override def iterator(): Iterator[(Int, Double)] = new Iterator[(Int, Double)] {
+    private var _count = 0
+
+    override def hasNext: Boolean = _count < dimension
+
+    override def next(): (Int, Double) = {
+      val ret = (_count, apply(_count))
+      _count += 1
+      ret
+    }
+  }
+
+  override def iteratorOnActives(): Iterator[(Int, Double)] = new Iterator[(Int, Double)] {
     private var _count = 0
 
     override def hasNext: Boolean = _count < data.length
@@ -52,5 +73,14 @@ case class SparseVector(override val dimension: Int, private val indices: Array[
       _count += 1
       ret
     }
+  }
+}
+
+object SparseVector {
+  def apply(dimension: Int, indices: Array[Int], data: Array[Double]): SparseVector = {
+    require(indices.length < dimension && (indices.length == data.length),
+      "Indices and data should have the same length < dimension.")
+    val sorted = indices.zip(data).sortBy(_._1)
+    new SparseVector(dimension, sorted.map(_._1), sorted.map(_._2))
   }
 }
--- src/test/scala/algebra/VectorsSuite.scala
+++ src/test/scala/algebra/VectorsSuite.scala
@@ -1,14 +1,40 @@
 package algebra
 
+import org.scalatest.FunSuite
+
 
 class VectorsSuite extends FunSuite {
 
-  test("basics") {
+  test("basics on dense") {
     val vec = DenseVector(Array(1d, 2d, 7d))
     assert(vec.dimension == 3)
     assert(vec(0) == 1d)
     assert(vec(1) == 2d)
     assert(vec(2) == 7d)
     assert(vec.iterator().toSeq == Seq((0, 1d), (1, 2d), (2, 7d)))
+    assert(vec.iteratorOnActives().toSeq == Seq((0, 1d), (1, 2d), (2, 7d)))
+  }
+
+  test("basics on sparse") {
+    val vec = SparseVector(5, Array(0, 1, 3), Array(1d, 2d, 7d))
+    assert(vec.dimension == 5)
+    assert(vec(0) == 1d)
+    assert(vec(1) == 2d)
+    assert(vec(2) == 0d)
+    assert(vec(3) == 7d)
+    assert(vec(4) == 0d)
+    assert(vec.iterator().toSeq == Seq((0, 1d), (1, 2d), (2, 0d), (3, 7d), (4, 0d)))
+    assert(vec.iteratorOnActives().toSeq == Seq((0, 1d), (1, 2d), (3, 7d)))
+
+    // with unsorted data as input
+    val vec2 = SparseVector(5, Array(0, 3, 1), Array(1d, 7d, 2d))
+    assert(vec2.dimension == 5)
+    assert(vec2(0) == 1d)
+    assert(vec2(1) == 2d)
+    assert(vec2(2) == 0d)
+    assert(vec2(3) == 7d)
+    assert(vec2(4) == 0d)
+    assert(vec2.iterator().toSeq == Seq((0, 1d), (1, 2d), (2, 0d), (3, 7d), (4, 0d)))
+    assert(vec2.iteratorOnActives().toSeq == Seq((0, 1d), (1, 2d), (3, 7d)))
   }
 }
